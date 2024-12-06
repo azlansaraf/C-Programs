caf9b48cf78f59ab0e42ba8f21edfcb68b99fc9b
azlansaraf <sarafazlan@gmail.com>
2024-12-06 21:50:50 +0530

    Initial

---
+++ College/PART-A/simple_interest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+void main () 
+{
+    float p,t,r,si;
+    printf("Enter the value of principle, time and rate of interest. ");
+    scanf("%f, %f, %f", &p, &t, &r);
+    si = (p*t*r)/100;
+    printf("\nThe simple interest is =%f \n", si);
+}
