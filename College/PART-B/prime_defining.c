670f1e9c19b7e6f3c7af2aa862b622e50799f9d8
azlansaraf <sarafazlan@gmail.com>
2024-12-07 19:38:23 +0530

    Initial

---
+++ College/PART-B/prime_defining.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int isprime(int n) {
+    int i;
+    for ( i = 2; i <= n/2; i++)
+    {
+        if(n%i == 0)
+                return 0;
+    }
+    return 1;
+    
+}
+void main () {
+    int num,res = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+ if (isprime(num))
+ {
+    printf("\n %d is a prime number",num);
+ }
+ else
+ {
+    printf("\n %d is not a prime number",num);
+ }
+ 
+    
+}
